4dca515d0b1cfcc2a4f7f89b2e73e1d9f6021f2a
Katee Coleman <kec141@pitt.edu>
2017-10-03 22:38:40 -0400

    Gave RentACatTest.java an outline

--- CommandLineJunit/RentACatTest.java
+++ CommandLineJunit/RentACatTest.java
@@ -6,5 +6,21 @@ import org.mockito.*;
 public static RentACatTest {
 
     // TESTS FOR RENTACAT.JAVA
+    // At least 1 test for each method
+    // ** 2 tests for each modified method **
+
+
+    // ** tests for "returnCat(Cat c)" **
+
+    // ** tests for "rentCat(Cat c)" **
+
+    // ** tests for "listCats(ArrayList<Cat> catList)" **
+
+    // ** tests for "catExists(int id, ArrayList<Cat> catList)" **
+
+    // tests for "catAvailable(int id, ArrayList<Cat> catList)"
+
+    // tests for "getCat(int id, ArrayList<Cat> catList)"
+
 
 }
